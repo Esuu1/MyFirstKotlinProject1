68f5432d22e883b4b500ad63e5bea7ec25745876
Activity4 <acebermoy@gmail.com>
2024-03-15 09:35:15 -0800

    Activity1

--- src/main/kotlin/Activity1.kt
+++ src/main/kotlin/Activity1.kt
@@ -3,13 +3,13 @@ fun main() {
 
 
     for(noOfGroup in 1..4) {
-        print("Enter your first name: ")
+        print("Enter your first name:")
         var first = readLine()
-        print("Enter your middle E: ")
+        print("Enter your middle E:")
         var E = readLine()
-        print("Enter your last name: ")
+        print("Enter your last name:")
         var last = readLine()
-        print("How old are you: ")
+        print("How old are you:")
         var age = readLine()?.toInt() ?: 0
 
         println("your name is $first $E. $last\nYour Age is $age")
