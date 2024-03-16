087e536a536e5f2b5a563d1136812cdc261fef5b
Activity4 <acebermoy@gmail.com>
2024-03-15 19:20:11 -0800

    ActivityB1: Library Borrow System

--- src/main/kotlin/ActivityB1.kt
+++ src/main/kotlin/ActivityB1.kt
@@ -1,176 +1,162 @@
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+val books = arrayListOf("ENGLISH", "MATH", "FILIPINO", "SCIENCE")
+val courses = arrayListOf("BSIT", "BSED", "BSBA", "BSHM", "BEED")
+
 fun main() {
-    var studentId: String? = null
-    var studentFname: String? = null
-    var studentMI: String? = null
-    var studentLName: String? = null
-    var studentName: String? = null
-    var continueAddStudent = true
-    var studentDept: String? = null
-    var yearLevelChoice: Int? = null
-    var yearLevel: String? = null
+    val borrowers = mutableListOf<Borrower>()
 
-    val subjects = arrayListOf("ENGLISH", "MATH", "FILIPINO", "SCIENCE")
+    var continueProgram = true
 
-    while (continueAddStudent) {
+    while (continueProgram) {
+        println("\nBook Borrower System")
+        println("[1] Add Borrower")
+        println("[2] Return Borrower")
+        println("[3] Display Borrowers")
+        println("[4] Exit")
+        print("Enter your choice: ")
+        val choice = readLine()?.toIntOrNull()
 
-        var continueAddBook = true
-        var borrowedBooks = ""
+        when (choice) {
+            1 -> {
+                val borrower = addBorrower()
+                borrowers.add(borrower)
+            }
+            2 -> {
+                returnBorrower(borrowers)
+            }
+            3 -> {
+                displayBorrowers(borrowers)
+            }
+            4 -> {
+                continueProgram = false
+                println("Exiting the Book Borrower System. Goodbye!")
+            }
+            else -> {
+                println("Invalid choice. Please try again.")
+            }
+        }
+    }
+}
 
-        while (continueAddBook) {
-            println("Welcome To Library System\n")
-            println("[1] Add Books")
-            println("[2] Remove Books")
-            println("[3] Borrow Books")
-            println("[4] Return Books")
-            println("[5] See Booklist")
+data class Borrower(
+    val studentId: String,
+    val studentName: String,
+    val yearLevel: String,
+    val course: String,
+    val subjects: MutableList<String>,
+    val borrowedBooks: MutableList<String>,
+    val borrowedDate: String
+)
 
-            print("\nEnter your choice: ")
-            val bookChoice = readLine()?.toIntOrNull()
+fun addBorrower(): Borrower {
+    println("\nAdding Borrower:")
+    print("Enter student's ID: ")
+    val studentId = readLine()?.let { "${it.substring(0, 2)}-${it.substring(2)}" } ?: ""
 
-            when (bookChoice) {
-                1 -> {
-                    print("Enter book name: ")
-                    val bookName = readLine()?.toUpperCase() ?: ""
-                    if (subjects.contains(bookName)) {
-                        Logger.log.error { "Book entered is already in the list." }
-                        println("Book '$bookName' is already in the list.")
-                    } else {
-                        subjects.add(bookName)
-                        Logger.log.info { "Book entered is added to the list." }
-                        println("Book '$bookName' added to the list.")
-                    }
-                }
+    print("Enter student's first name: ")
+    val studentFname = readLine()?.toUpperCase() ?: ""
+    print("Enter student's Middle Name: ")
+    val studentMI = readlnOrNull()?.toCharArray()?.getOrNull(0)?.toUpperCase()
+    print("Enter student's last name: ")
+    val studentLName = readLine()?.toUpperCase() ?: ""
+    val studentName = "$studentFname ${studentMI ?: ""}. $studentLName"
 
-                2 -> {
-                    print("Enter book name to remove: ")
-                    val bookToRemove = readLine()?.toUpperCase() ?: ""
-                    if (subjects.contains(bookToRemove)) {
-                        subjects.remove(bookToRemove)
-                        Logger.log.info{ "Book is removed from the list." }
-                        println("Book '$bookToRemove' removed from the list.")
-                    } else {
-                        Logger.log.error{"Book entered is not in the list."}
-                        println("Book '$bookToRemove' is not in the list.")
-                    }
-                }
+    println("Enter student's course:")
+    courses.forEachIndexed { index, course -> println("${index + 1}. $course") }
+    print("Enter your choice: ")
+    val courseChoice = readLine()?.toIntOrNull()
+    val course = courses.getOrNull(courseChoice?.minus(1) ?: -1) ?: ""
 
-                3 -> {
-                    print("Enter student's ID: ")
-                    studentId = readLine()?.let { "${it.substring(0, 2)}-${it.substring(2)}" }
+    println("Enter student's year level:")
+    println("1. First year")
+    println("2. Second year")
+    println("3. Third year")
+    println("4. Fourth year")
+    print("Enter your choice: ")
+    val yearLevelChoice = readLine()?.toIntOrNull()
 
-                    print("Enter student's first name: ")
-                    studentFname = readLine()?.toUpperCase()
+    val yearLevel = when (yearLevelChoice) {
+        1 -> "First year"
+        2 -> "Second year"
+        3 -> "Third year"
+        4 -> "Fourth year"
+        else -> "Unknown"
+    }
 
-                    print("Enter student's Middle Name: ")
-                    studentMI = readLine()?.toCharArray()?.getOrNull(0)?.toUpperCase()?.toString()
+    val borrowedBooks = mutableListOf<String>()
+    var continueAddBook = true
 
-                    print("Enter student's last name: ")
-                    studentLName = readLine()?.toUpperCase()
-                    studentName = "$studentFname $studentMI. $studentLName"
+    while (continueAddBook) {
+        println("\nAvailable Books to Borrow:")
+        books.forEachIndexed { index, book -> println("${index + 1}. $book") }
+        print("Enter the book to borrow (or enter 'exit' to finish adding books): ")
+        val borrowedBook = readLine()?.toUpperCase()
 
-                    print("Enter Department: ")
-                    studentDept = readLine()?.toUpperCase()
-                    println("Enter student's year level:")
-                    println("1. First year")
-                    println("2. Second year")
-                    println("3. Third year")
-                    println("4. Fourth year")
-                    print("Enter your choice: ")
-                    yearLevelChoice = readLine()?.toIntOrNull()
-
-                    yearLevel = when (yearLevelChoice) {
-                        1 -> "First year"
-                        2 -> "Second year"
-                        3 -> "Third year"
-                        4 -> "Fourth year"
-                        else -> "Unknown"
-                    }
-
-                    // Initialize continueAddBook to true to enter the borrowing loop
-                    continueAddBook = true
-
-                    // Start the borrowing loop
-                    while (continueAddBook) {
-                        print("Enter book name to borrow: ")
-                        var borrowBook = readLine()?.toUpperCase() ?: ""
-
-                        while (!subjects.contains(borrowBook)) {
-                            println("Book is not present. Enter another book name.")
-                            borrowBook = readLine()?.toUpperCase() ?: ""
-                        }
-
-                        borrowedBooks += if (borrowedBooks.isEmpty()) borrowBook else ", $borrowBook"
-
-                        println("Is book '$borrowBook' available: (yes/no)")
-                        val isAvailable = readLine()?.toLowerCase() == "yes"
-
-                        if (isAvailable) {
-                            println("Book '$borrowBook' is available. Borrowed!")
-                            // Display student details after borrowing a book
-                            Logger.log.info { "Data Saved Successfully!" }
-                            displayStudentDetails(studentId, studentName, studentDept, yearLevel, borrowedBooks)
-                        } else {
-                            Logger.log.warn { "User entered book is unavailable." }
-                            println("Book '$borrowBook' is unavailable.")
-                        }
-
-                        println("\nDo you want to add another book to borrow? (yes/no)")
-                        continueAddBook = readLine()?.toLowerCase() == "yes"
-                    }
-                }
-
-                4 -> {
-                    if (borrowedBooks.isNotEmpty()) {
-                        print("Enter book name to return: ")
-                        val returnBook = readLine()?.toUpperCase() ?: ""
-
-                        if (borrowedBooks.contains(returnBook)) {
-                            borrowedBooks = borrowedBooks.replace(returnBook, "").replace(", ,", ",")
-                            Logger.log.info { "User's borrowed book returned successfully." }
-                            println("Book '$returnBook' returned successfully.")
-                        } else {
-                            Logger.log.warn { "Book user entered was not borrowed." }
-                            println("Book '$returnBook' was not borrowed.")
-                        }
-                    } else {
-                        Logger.log.error{"No books borrowed."}
-                        println("No books borrowed.")
-                    }
-                }
-
-                5 -> {
-                    if (subjects.isEmpty()) {
-                        Logger.log.warn{"books are not available."}
-                        println("No books available.")
-                    } else {
-                        println("Available Books:")
-                        subjects.forEachIndexed { index, book ->
-                            println("${index + 1}. $book")
-                        }
-                    }
-                }
-
-                else -> {
-                    Logger.log.error{"User choose invalid number."}
-                    println("Invalid Choice. Enter a valid number.")
+        if (borrowedBook == "EXIT") {
+            continueAddBook = false
+        } else {
+            if (borrowedBook !in books) {
+                println("Book '$borrowedBook' is not available.")
+            } else if (borrowedBook in borrowedBooks) {
+                println("Book '$borrowedBook' already added.")
+            } else {
+                println("Book '$borrowedBook' added.")
+                if (borrowedBook != null) {
+                    borrowedBooks.add(borrowedBook)
                 }
             }
         }
-
-        println("\nDo you want to add another student to borrow books? (yes/no)")
-        continueAddStudent = readLine()?.toLowerCase() == "yes"
     }
-    Logger.log.info{"System Exiting."}
-    println("Exiting the Library System. Thank You!")
-}
 
-fun displayStudentDetails(studentId: String?, studentName: String?, studentDept: String?, yearLevel: String?, borrowedBooks: String) {
     val currentDate = LocalDate.now()
     val formattedDate = currentDate.format(DateTimeFormatter.ofPattern("MMMM dd, yyyy"))
 
-    println("\nStudent Details:")
-    println("Student ID: $studentId, Student Name: $studentName, Department: $studentDept, Year Level: $yearLevel, Date: $formattedDate, Books: $borrowedBooks")
+    return Borrower(studentId, studentName, yearLevel, course, mutableListOf(), borrowedBooks, formattedDate)
 }
+
+
+fun returnBorrower(borrowers: MutableList<Borrower>) {
+    if (borrowers.isNotEmpty()) {
+        println("\nReturning Borrower:")
+        println("Enter student's ID to return: ")
+        val studentId = readLine()
+        val borrower = borrowers.find { it.studentId == studentId }
+
+        if (borrower != null) {
+            borrowers.remove(borrower)
+            println("Borrower with ID '$studentId' returned successfully.")
+        } else {
+            println("Borrower with ID '$studentId' not found.")
+        }
+    } else {
+        println("\nNo borrowers to return.")
+    }
+}
+
+fun displayBorrowers(borrowers: List<Borrower>) {
+    if (borrowers.isNotEmpty()) {
+        println("\nList of Borrowers:")
+        for ((index, borrower) in borrowers.withIndex()) {
+            println("Borrower ${index + 1}:")
+            println("Student ID: ${borrower.studentId}")
+            println("Student Name: ${borrower.studentName}")
+            println("Course: ${borrower.course}")
+            println("Year Level: ${borrower.yearLevel}")
+            println("Borrowed Date: ${borrower.borrowedDate}")
+            if (borrower.borrowedBooks.isNotEmpty()) {
+                println("Borrowed Books: ${borrower.borrowedBooks.joinToString(", ")}")
+            } else {
+                Logger.log.error { "Books are not borrowed." }
+                println("No books borrowed.")
+            }
+            println()
+        }
+    } else {
+        Logger.log.error { "No displayed borrowers." }
+        println("\nNo borrowers to display.")
+    }
+}
+
+fun readlnOrNull() = readLine()
